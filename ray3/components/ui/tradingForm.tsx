bb32fd7c681742bad36c542b4c68c4ae1e755eb7
Nandy Bâ <nandyba@yahoo.fr>
2023-10-15 05:46:57 +0200

    Add balance and amount max button

--- ray3/components/ui/tradingForm.tsx
+++ ray3/components/ui/tradingForm.tsx
@@ -1,12 +1,31 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
+import { get_balance_user_friendly } from '../../lib/contracts/fa2'
 
-const TradingForm = () => {
+const TradingForm = ({
+  Tezos,
+  setTezos,
+  setContract,
+  setWallet,
+  setUserAddress,
+  userAddress,
+  setUserBalance,
+  setStorage,
+  contractAddress,
+  setBeaconConnection,
+  setPublicToken,
+  wallet,
+}) => {
   const [position, setPosition] = useState<'long' | 'short'>('long'); // Par défaut, position long
   const [amount, setAmount] = useState(0);
   const [leverage, setLeverage] = useState(1);
+  const [tokenBalance, setTokenBalance] = useState(0);
 
   const handlePositionChange = (selectedPosition: 'long' | 'short') => {
     setPosition(selectedPosition);
+  };
+
+  const handleMaxAmount = () => {
+    setAmount(tokenBalance);
   };
 
   const handleAmountChange = (e: React.ChangeEvent<HTMLInputElement>) => {
@@ -24,6 +43,24 @@ const TradingForm = () => {
     // Logique pour exécuter l'ordre en fonction des valeurs de position, amount et leverage.
     console.log('Ordre exécuté :', position, amount, leverage);
   };
+
+  useEffect(() => {
+    if(!Tezos || !userAddress) return;
+    (async () => {
+      const fa2_contract_address = "KT19QkeKzaHaAJGSRqH8ijF1pkgbvTNQkjfU"; // token id 0: uUSD testnet
+      const balance = await get_balance_user_friendly(Tezos, fa2_contract_address, userAddress);
+      console.log({balance})
+      setTokenBalance(balance.toString());
+    }
+    )();
+  },[Tezos, userAddress]);
+
+  useEffect(() => {
+    if(isNaN(amount) || amount === undefined) return;
+    if(amount > tokenBalance) return setAmount(tokenBalance);
+    if(amount < 0) return setAmount(0);
+
+  }, [tokenBalance, amount]);
 
   return (
     <div className="trading-form">
@@ -45,15 +82,24 @@ const TradingForm = () => {
       </div>
       <div className="mb-10">
         <label>Amount (USDT):</label>
-        <input
-          type="number"
-          value={amount}
-          onChange={handleAmountChange}
-          className="w-full p-2"
-          style={{
-            color: 'black',
-          }}
-        />
+        <div className="flex justify-between">
+          <input
+            type="number"
+            value={amount}
+            onChange={handleAmountChange}
+            className="w-full p-2"
+            style={{
+              color: 'black',
+            }}
+          />
+          <a 
+            onClick={handleMaxAmount} 
+            className="text-blue-500 cursor-pointer ml-2"
+            style={{ alignSelf: 'center' }}
+          >
+            Max
+          </a>
+        </div>
       </div>
       <div className="mb-10 leverage-slider">
         <label>Leverage:</label>
